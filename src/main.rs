8ba5bf3f5e1406b88d7554d93d8019b6daa88324
Fransisco Fernandez Castano <francisco.castano@datastax.com>
2018-03-27 23:56:48 +0200

    Adding error_chain.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
+#![recursion_limit = "1024"]
+
+#[macro_use]
+extern crate error_chain;
 extern crate nix;
 extern crate rustyline;
-
-mod parser;
 
 use nix::sys::wait::waitpid;
 use nix::unistd::{execvp, fork, ForkResult};
 use rustyline::Editor;
 use rustyline::error::ReadlineError;
 
-fn rshell_loop() {
+mod parser;
+mod errors {
+    // Create the Error, ErrorKind, ResultExt, and Result types
+    error_chain!{}
+}
+
+use errors::*;
+
+fn rshell_loop() -> Result<()> {
     let mut rl = Editor::<()>::new();
     if let Err(_) = rl.load_history("history.txt") {
         println!("No previous history.");
@@ -40,9 +50,23 @@ fn rshell_loop() {
             }
         }
     }
-    rl.save_history("history.txt").unwrap();
+    rl.save_history("history.txt")
+        .chain_err(|| "unable to save history.txt file")?;
+    Ok(())
 }
 
 fn main() {
-    rshell_loop();
+    if let Err(ref e) = rshell_loop() {
+        println!("error: {}", e);
+
+        for e in e.iter().skip(1) {
+            println!("caused by: {}", e);
+        }
+
+        if let Some(backtrace) = e.backtrace() {
+            println!("backtrace: {:?}", backtrace);
+        }
+
+        ::std::process::exit(1);
+    }
 }
--- src/parser/rcommand.rs
+++ src/parser/rcommand.rs
@@ -10,10 +10,9 @@ impl RCommand {
     pub fn new(buffer: &String) -> RCommand {
         let cmd = buffer.replace("\n", "");
         let cmd_vec: Vec<CString> = cmd.split(" ").map(|s| CString::new(s).unwrap()).collect();
-        let args: Vec<CString> = cmd_vec[1..cmd_vec.len()].to_vec();
         RCommand {
             bin: cmd_vec[0].clone(),
-            args: args,
+            args: cmd_vec,
             background: false,
         }
     }
